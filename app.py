3a0c4530261d15c68ab9531d6f3cb3fb2053d037
Viet Ha Nguyen <hanv.cotai@gmail.com>
2023-09-28 14:17:49 +0700

    tracking & call api

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import io
-import sys
 import cv2
 import time
 import json
 import requests
+import queue
+import threading
 import numpy as np
 from PIL import Image
 from helper import DamageHelper
+from tracker import track
 
 MAX_CALL = 3
 API_URL = 'https://api.platerecognizer.com/v1/plate-reader/'
 HEADER={'Authorization': 'Token 45f3172a25b6ea562e6174ac2475b7ca26b8e2fc'}
+left, top, right, bottom = 400, 400, 700, 665   # ROI
 
 def call_plate_recognizer_api(frame):
     byte_io = io.BytesIO()
@@ -21,44 +24,56 @@ def call_plate_recognizer_api(frame):
     byte_io.close()
     return response
 
-def read_file():
-    roi = [400, 400, 700, 665]
-    left, top, right, bottom = roi
-    cap = cv2.VideoCapture('video.avi')
-    calls = MAX_CALL
+def main():
     model = DamageHelper('yolov5s_openvino_model/yolov5s.xml')
-    last_call = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
+    cap = cv2.VideoCapture('video.avi')
+    q=queue.Queue()
+    stop = False
+    def capture():
+        while not stop:
+            ret, frame = cap.read()
+            if ret:q.put(frame)
+            else:break
+
+    thread = threading.Thread(target=capture)
+    thread.start()
+    time.sleep(5)
+
+    tracking_info = None
+    while not q.empty():
         # t = time.time()
+        frame = q.get()
         key = cv2.waitKey(1)
-        if key == ord("q") or not ret:
-            break
+        if key == ord("q"):
+            stop = True
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # results = model.process(frame, 416) # detect all frame
-        results = model.process(frame[top:bottom, left:right], 416) # detect in ROI only
+        cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 3)
+        roi = frame[top:bottom, left:right]
+        results = model.process(roi, 416) # detect in ROI only
         if results is not None:
             results = [p for p in results if p[-1] == 2] # car
-            for l,t,r,b,_,_ in results:
-                if (r-l)*(b-t) >= 30000:
-                    cv2.rectangle(frame[top:bottom, left:right], (l,t), (r,b), (0,0,255), 3)
-                    cur_time = time.time()
-                    if cur_time - last_call > 5:
-                        print('reset calls')
-                        calls = MAX_CALL
-                    if calls > 0 and cur_time - last_call > 2:
-                        last_call = time.time()
-                        calls -= 1
-                        response = call_plate_recognizer_api(frame[top:bottom, left:right])
-                        if response.json()['results']:
-                            plate = response.json()['results'][0]['plate']
-                            print(plate)
+        if results:
+            # results = np.array(results, dtype=float)
+            detections = track(roi, np.array(results, dtype=float))
+            for d in detections:
+                if d.rect.width * d.rect.height >= 30000:
+                    x1, y1, x2, y2 = int(d.rect.x), int(d.rect.y), int(d.rect.max_x), int(d.rect.max_y)
+                    cv2.rectangle(roi, (x1, y1), (x2, y2), (255, 0, 0), 3)
+                    cv2.putText(roi, f'{d.tracker_id}', org=(x1+2, y1+15), fontFace = cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=.5, color=(0, 255, 0), thickness=2)
+                    if tracking_info is None or tracking_info['id'] != d.tracker_id:
+                        tracking_info = {'id':d.tracker_id, 'waiting':False, 'calls':MAX_CALL}
+                    print(tracking_info)
+                    if not tracking_info['waiting'] and tracking_info['calls'] > 0:
+                        tracking_info['waiting'] = True
+                        tracking_info['calls'] -= 1
+                        response = call_plate_recognizer_api(roi)
+                        print(response.json())
 
-        cv2.rectangle(frame, (roi[0], roi[1]), (roi[2], roi[3]), (0,255,0), 3)
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         cv2.imshow('frame', frame)
 
     cap.release()
     cv2.destroyAllWindows()
 
-read_file()
+main()
