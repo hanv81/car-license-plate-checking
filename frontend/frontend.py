9340974129b0773c92f21646aa0c7b7278259b4b
Viet Ha Nguyen <hanv.cotai@gmail.com>
2023-10-25 16:17:11 +0700

    get config

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -76,60 +76,64 @@ def main():
                     st.image(img, h[0] + ' - ' + str(dt_object))
             else:
                 st.write(response.json())
+
         with tab3:
-            response = requests.get(url = API_URL + 'get_config')
-            configs = response.json()
-            with st.expander('Config', True):
-                roi = list(map(int, configs['roi'].split()))
-                cols = st.columns(5)
-                with cols[0]:
-                    left = st.number_input('Left', value=roi[0])
-                with cols[1]:
-                    top = st.number_input('Top', value=roi[1])
-                with cols[2]:
-                    right = st.number_input('Right', value=roi[2])
-                with cols[3]:
-                    bottom = st.number_input('Bottom', value=roi[3])
-                with cols[4]:
-                    obj_size = st.number_input('Object size', value=int(configs['obj_size']))
-                
-                col1, col2 = st.columns(2)
-                with col1:file = st.selectbox('File', os.listdir('video'))
-                cap = cv2.VideoCapture('video/' + file)
-                while cap.isOpened():
-                    _, frame = cap.read()
-                    if frame is not None:
-                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                        with col2:
-                            st.text('Frame Size')
-                            st.write(frame.shape[:2])
-                        cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
-                        img = Image.fromarray(frame)
-                        st.image(img)
-                        break
-                cap.release()
+            response = requests.get(url = API_URL + 'get_config', headers = headers)
+            if response.status_code != HTTPStatus.OK:
+                st.error(response.json()['detail'])
+            else:
+                configs = response.json()
+                with st.expander('Config', True):
+                    roi = list(map(int, configs['roi'].split()))
+                    cols = st.columns(5)
+                    with cols[0]:
+                        left = st.number_input('Left', value=roi[0])
+                    with cols[1]:
+                        top = st.number_input('Top', value=roi[1])
+                    with cols[2]:
+                        right = st.number_input('Right', value=roi[2])
+                    with cols[3]:
+                        bottom = st.number_input('Bottom', value=roi[3])
+                    with cols[4]:
+                        obj_size = st.number_input('Object size', value=int(configs['obj_size']))
+                    
+                    col1, col2 = st.columns(2)
+                    with col1:file = st.selectbox('File', os.listdir('video'))
+                    cap = cv2.VideoCapture('video/' + file)
+                    while cap.isOpened():
+                        _, frame = cap.read()
+                        if frame is not None:
+                            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                            with col2:
+                                st.text('Frame Size')
+                                st.write(frame.shape[:2])
+                            cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
+                            img = Image.fromarray(frame)
+                            st.image(img)
+                            break
+                    cap.release()
 
-                cols = st.columns(2)
-                success = True
-                update_code = None
-                with cols[0]:
-                    if st.button('Update', use_container_width=True):
-                        response = requests.post(url = API_URL + 'update_config', headers = headers,
-                                                 params = {'file': file,
-                                                           'roi': f'{left} {top} {right} {bottom}',
-                                                           'obj_size': f'{obj_size}'})
-                        update_code = response.status_code
-                if update_code is not None:
-                    if update_code == HTTPStatus.OK:
-                        st.success("Config updated") 
-                    else:
-                        st.error(response.json()['detail'])
+                    cols = st.columns(2)
+                    success = True
+                    update_code = None
+                    with cols[0]:
+                        if st.button('Update', use_container_width=True):
+                            response = requests.post(url = API_URL + 'update_config', headers = headers,
+                                                     params = {'file': file,
+                                                               'roi': f'{left} {top} {right} {bottom}',
+                                                               'obj_size': f'{obj_size}'})
+                            update_code = response.status_code
+                    if update_code is not None:
+                        if update_code == HTTPStatus.OK:
+                            st.success("Config updated") 
+                        else:
+                            st.error(response.json()['detail'])
 
-                with cols[1]:
-                    if st.button('Start', use_container_width=True):
-                        success = start_app()
-                if not success:
-                    st.error('App started failed')
+                    with cols[1]:
+                        if st.button('Start', use_container_width=True):
+                            success = start_app()
+                    if not success:
+                        st.error('App started failed')
 
         with tab4:
             response = requests.post(API_URL+'statistic', headers=headers)
