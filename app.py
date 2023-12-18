b89919c7bd149e600d79663e6cd617d1ff7d303f
Viet Ha Nguyen <hanv.cotai@gmail.com>
2023-12-18 14:13:18 +0700

    boxmot tracker

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import numpy as np
 from config import Config
 import screeninfo
 from PIL import Image
-from tracker import track, Detection
 from model import CustomModel
+from boxmot import BYTETracker
 
 MAX_CALL = 3
 
@@ -15,16 +15,16 @@ def call_backend_api(url, frame, bbox):
     byte_io.close()
     return response
 
-def check_detection(url:str, frame:np.ndarray, d:Detection, tracking:dict):
-    bbox = str(int(d.rect.x)), str(int(d.rect.y)), str(int(d.rect.max_x)), str(int(d.rect.max_y))
+def check_detection(url:str, frame:np.ndarray, bbox, id, tracking:dict):
     response = call_backend_api(url, frame, ' '.join(bbox))
     tracking['waiting'] = False
-    print('server response', response.json(), d.tracker_id)
+    print('server response', response.json(), id)
     tracking['info'] = response.json().get('msg')
     if response.json().get('identified'):
         tracking['done'] = True
 
 def main():
+    tracker = BYTETracker(track_thresh = 0.25)
     model = CustomModel()
     cf = Config()
     left, top, right, bottom = cf.roi
@@ -63,7 +63,7 @@ def main():
         elif key == ord('r'):
             resize = not resize
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        process_roi(frame[top:bottom, left:right], model, cf, tracking_info, draw_bbox)
+        track_roi(frame[top:bottom, left:right], model, tracker, cf, tracking_info, draw_bbox)
         if draw_bbox:
             cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
 
@@ -93,30 +93,34 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
-def process_roi(roi, model, cf, tracking_info, draw_bbox):
+def track_roi(roi:np.ndarray, model: CustomModel, tracker:BYTETracker, config:Config, tracking_info:dict, draw_bbox:bool):
     preds = model(roi, classes=[2])
-    if len(preds) > 0:
-        roi_api = roi.copy()    # fix frame checking with bbox
-        detections = track(roi, np.array(preds, dtype=float))
-        for d in detections:
-            if d.tracker_id is not None and d.rect.width * d.rect.height >= cf.obj_size:
-                tracking = tracking_info.get(d.tracker_id)
-                if tracking is None:
-                    tracking = {'waiting':False, 'done':False, 'calls':MAX_CALL, 'info':''}
-                    tracking_info[d.tracker_id] = tracking
+    if len(preds) == 0:
+        return
+    
+    roi_api = roi.copy()
+    tracks = tracker.update(np.array(preds), roi)
+    for t in tracks:
+        x1, y1, x2, y2, id = list(map(int, t[:5]))
+        if (x2-x1) * (y2-y1) < config.obj_size:
+            continue
 
-                if not tracking['done'] and not tracking['waiting'] and tracking['calls'] > 0:
-                    tracking['waiting'] = True
-                    tracking['calls'] -= 1
-                    threading.Thread(target=check_detection, args=(cf.backend_url, roi_api, d, tracking)).start()
-                    # time.sleep(.5)
+        tracking = tracking_info.get(id)
+        if tracking is None:
+            tracking = {'waiting':False, 'done':False, 'calls':MAX_CALL, 'info':''}
+            tracking_info[id] = tracking
 
-                if draw_bbox:
-                    info = tracking['info']
-                    x1, y1, x2, y2 = int(d.rect.x), int(d.rect.y), int(d.rect.max_x), int(d.rect.max_y)
-                    cv2.rectangle(roi, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                    cv2.putText(roi, f'{d.tracker_id} {info}', org=(x1+2, y1+17), 
-                                fontFace = cv2.FONT_HERSHEY_SIMPLEX, 
-                                fontScale=.8, color=(0, 255, 0), thickness=2)
+        if not tracking['done'] and not tracking['waiting'] and tracking['calls'] > 0:
+            tracking['waiting'] = True
+            tracking['calls'] -= 1
+            threading.Thread(target=check_detection, 
+                             args=(config.backend_url, roi_api, map(str, (x1,y1,x2,y2)), id, tracking)
+                            ).start()
+
+        if draw_bbox:
+            info = tracking['info']
+            cv2.rectangle(roi, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.putText(roi, f'{id} {info}', org=(x1+2, y1+17), 
+                        fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale=.8, color=(0, 255, 0), thickness=2)
 
 main()
